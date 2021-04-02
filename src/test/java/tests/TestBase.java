3022e09b3f8a53fd0201a46b570df0f120b34c3f
Valkyrja <kilyanet@gmail.com>
2021-04-02 14:53:34 +0300

    Work with cookie added

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -1,5 +1,6 @@
 package tests;
 
+import com.codeborne.selenide.Configuration;
 import io.restassured.RestAssured;
 import org.junit.jupiter.api.BeforeAll;
 
@@ -8,5 +9,6 @@ public class TestBase {
     @BeforeAll
     static void setup(){
         RestAssured.baseURI = "http://demowebshop.tricentis.com";
+        Configuration.baseUrl = "http://demowebshop.tricentis.com";
     }
 }
